9bdb63a8b183bea48e2b05d8512c4f93ab31798a
Guilherme Nunes <guilherme.martins@plugg.to>
2021-04-04 08:26:19 -0300

    Added minimal html to the index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,18 @@ import Head from 'next/head';
 export default function Home() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <Head>
+        <title>Catalog - Trustly Frontend Challenge</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Sneakers application for the Trustly Frontend Challenge"
+        />
+      </Head>
+
+      <main>
+        <h1>Hello world.</h1>
+      </main>
     </div>
   )
 }
