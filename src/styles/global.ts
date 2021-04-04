fd0d1a791fded2aca6762ad7e26e91e478a4c8ab
Guilherme Nunes <guilherme.martins@plugg.to>
2021-04-04 08:17:31 -0300

    Added styled-components and a GlobalStyle

---
+++ src/styles/global.ts
@@ -0,0 +1,49 @@
+// globalStyles.js
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+
+  :root {
+    font-size: 60%;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    height: 100vh;
+  }
+
+  body {
+    background: #fff;
+  }
+
+  #root {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  body,
+  input,
+  button,
+  textarea {
+    font: 500 1.6rem Open-Sans, Helvetica, Sans-Serif;
+  }
+
+  .container {
+    width: 90vw;
+    max-width: 700px;
+  }
+
+  @media (min-width: 700px) {
+    :root {
+      font-size: 62.5%;
+    }
+  }
+`;
+
+export default GlobalStyle;
