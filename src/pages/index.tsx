e500ebdc1dd465a337a329fe0892f6ebf226c6ed
Guilherme Nunes <guilherme.martins@plugg.to>
2021-04-05 14:20:15 -0300

    Added Header component to Catalog page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Header from '../components/UI/Header';
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
           content="Sneakers application for the Trustly Frontend Challenge"
         />
       </Head>
-
+      <Header title={'Sneakers'} previous={true} />
       <main>
         <h1>Hello world.</h1>
       </main>
