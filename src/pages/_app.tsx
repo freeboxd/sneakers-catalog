f468c61a6ac7cfa5aa4cf3d4c489445f56168432
Guilherme Nunes <guilherme.martins@plugg.to>
2021-04-07 02:13:06 -0300

    Added "Open Sans" from Google Fonts to the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ function App({ Component, pageProps }) {
       <ThemeProvider theme={theme}>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap"
             rel="stylesheet"
           />
         </Head>
