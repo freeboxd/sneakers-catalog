7b3e8b2fc445b8985f2a9335611b281b640c249e
Guilherme Nunes <guilherme.martins@plugg.to>
2021-04-07 01:44:16 -0300

    Implemented Card UI component

---
+++ src/components/UI/Card/index.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Card = styled.div`
+  box-shadow: 0px 6px 16px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+`;
+
+export const CardBody = styled.div `
+  padding: 2.1rem;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`;
