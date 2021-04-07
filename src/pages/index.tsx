6527e9b6fe154499f5585990e5df9178ac435696
Guilherme Nunes <guilherme.martins@plugg.to>
2021-04-07 02:25:07 -0300

    Finished Sneakers page styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,62 @@
+import React, { useEffect, useCallback, useState } from 'react';
 import Head from 'next/head';
 import Header from '../components/UI/Header';
+import Input from '../components/UI/Input';
+import { Search } from '@styled-icons/bootstrap/Search';
+import SneakerListItem from '../components/SneakerListItem';
+import axios from 'axios';
+import { Sneaker } from '../@types/index';
+
+import {
+  MainContainer,
+  ProductList,
+  ProductListContainer,
+  NotFoundMessage,
+} from './styles';
 
 export default function Home() {
+  const [sneakersOriginalList, setSneakersOriginalList] = useState<Sneaker[]>([]);
+  const [sneakersList, setSneakersList] = useState<Sneaker[]>([]);
+  const [searchValue, setSearchValue] = useState<String>('');
+
+  const getSneakers = async () => {
+    const result = await axios
+      .get('/sneakers.json')
+      .then(res => res)
+      .catch(err => {
+        console.log(err.response);
+        return err.response;
+      });
+    return result;
+  };
+
+  // Fetches the sneakers list and populates the components' arrays
+  // if successful
+  useEffect(() => {
+    (async () => {
+      const res = await getSneakers();
+      if (res && res.status === 200) {
+        setSneakersOriginalList(res.data.results);
+        setSneakersList(res.data.results);
+      }
+    })();
+  }, []);
+
+  // Deals with the search bar filtering
+  const handleSearchChange = useCallback(
+    (value: string) => {
+      setSearchValue(value);
+      const filteredSneakers = sneakersOriginalList.filter(sneaker =>
+        sneaker.description
+          .toLowerCase()
+          .includes(value.length > 0 ? value.toLocaleLowerCase() : ''),
+      );
+      setSneakersList(filteredSneakers);
+    }, [sneakersList, sneakersOriginalList],
+  );
+
   return (
-    <div>
+    <>
       <Head>
         <title>Catalog - Trustly Frontend Challenge</title>
         <link rel="icon" href="/favicon.ico" />
@@ -12,10 +65,38 @@ export default function Home() {
           content="Sneakers application for the Trustly Frontend Challenge"
         />
       </Head>
-      <Header title={'Sneakers'} previous={true} />
+      <Header
+        title="Sneaker"
+        previous={true}
+      />
       <main>
-        <h1>Hello world.</h1>
+        <MainContainer>
+          <Input
+            name="Search"
+            icon={Search}
+            placeholder="Search for your sneaker"
+            onChange={e => handleSearchChange(e.target.value)}
+          />
+
+          <ProductList>
+              {sneakersList.length > 0 ? (
+                <ProductListContainer>
+                  {sneakersList.map((sneaker: Sneaker) => (
+                    <SneakerListItem
+                      sneaker={sneaker}
+                      key={sneaker.id}
+                    />
+                  ))}
+                </ProductListContainer>
+                ) : (
+                  <NotFoundMessage>
+                    There are no sneakers that match {searchValue}.
+                  </NotFoundMessage>
+                )
+              }
+          </ProductList>
+        </MainContainer>
       </main>
-    </div>
+    </>
   )
 }
---
+++ src/pages/styles.ts
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+import {Card} from '../components/UI/Card';
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  width: 90%;
+  padding-bottom: 6rem;
+
+  @media all and (min-width: 769px) {
+    margin-top: 4rem;
+  }
+`;
+
+export const ProductList = styled.div`
+  width: 100%;
+`;
+
+export const ProductListContainer = styled.div`
+  width: 85%;
+  margin: 0 auto;
+  padding-top: 6.5rem;
+  column-gap: 5rem;
+
+  & ${Card} + ${Card} {
+    margin-top: 6rem;
+  }
+
+  @media all and (min-width: 769px) {
+    width: 94%;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    row-gap: 50px;
+    
+    & ${Card} + ${Card} {
+      margin-top: 0;
+    }
+  }
+`;
+
+export const NotFoundMessage = styled.h3`
+  margin-top: 3rem;
+  font-size: 2rem;
+  text-align: center;
+  color: ${props => props.theme.color.primary};
+  font-weight: 600;
+`;
