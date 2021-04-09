12e42a79f4d38e4f77ac6c1e260880116fd1f07d
Guilherme Nunes <guilherme.martins@plugg.to>
2021-04-09 08:13:39 -0300

    Minor styles adjustment

--- src/components/Checkout/PaymentOptions/styles.ts
+++ src/components/Checkout/PaymentOptions/styles.ts
@@ -60,7 +60,7 @@ export const CartItemInfo = styled.div`
 `;
 
 export const DeliveryDetailsContainer = styled.div`
-  width: 50%;
+  width: 100%;
   margin-top: 1.6rem;
 
   @media all and (min-width: 769px) {
