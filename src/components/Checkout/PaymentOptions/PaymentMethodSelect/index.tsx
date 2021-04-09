426d3e1d17fecde77728748df9ff35eabf97c460
Guilherme Nunes <guilherme.martins@plugg.to>
2021-04-09 01:32:01 -0300

    Created PaymentMethodSelect Checkout component

---
+++ src/components/Checkout/PaymentOptions/PaymentMethodSelect/index.tsx
@@ -0,0 +1,85 @@
+import React, { useCallback } from 'react';
+import { useEffect } from 'react';
+import { IPaymentMethod } from '../../../../@types/payment/Payment';
+import useCart from '../../../../hooks/cart';
+import AlternativeSelect, {IAlternativeSelectOption} from '../../../UI/AlternativeSelect';
+import {BuildingBank} from '@styled-icons/fluentui-system-filled/BuildingBank';
+import {CreditCard} from '@styled-icons/bootstrap/CreditCard';
+import {CcApplePay} from '@styled-icons/fa-brands/CcApplePay';
+import {
+  Container,
+  HeaderTitle,
+  PaymentMethodsContainer,
+} from './styles';
+
+const PaymentOptionsSelect: React.FC = () => {
+  const { changeSelectedPaymentMethod } = useCart();
+
+  const paymentMethods: IPaymentMethod[] = [
+    {
+      name: 'Online Banking',
+      badge: 'Save $10',
+      iconSrc: 'online_banking.png',
+      id: 'online-banking',
+      icon: BuildingBank,
+    },
+    {
+      name: 'Card Payment',
+      iconSrc: 'mastercard_visa.png',
+      id: 'card-payment',
+      icon: CreditCard,
+    },
+    {
+      name: 'Apple Pay',
+      iconSrc: 'apple_pay.png',
+      id: 'apple-play',
+      icon: CcApplePay,
+    },
+  ];
+
+  useEffect(() => {
+    changeSelectedPaymentMethod(paymentMethods[0]);
+  }, []);
+  
+  const mapAllPaymentMethodsToAlternativeSelectOption = useCallback(
+    () => {
+      const options = paymentMethods
+        .map(({ name, id, iconSrc, badge }) => {
+          const alternativeSelectOption: IAlternativeSelectOption = { title: name, id, iconSrc };
+
+          if (badge) {
+            alternativeSelectOption.badge = badge;
+          }
+
+          return alternativeSelectOption;
+        });
+
+        return options;
+
+    }, [paymentMethods],
+  );
+
+  const handleChangePaymentMethod = useCallback(
+    (option: IAlternativeSelectOption) => {
+      const paymentMethod: IPaymentMethod = paymentMethods.find((pm) => pm.id === option.id);
+
+      changeSelectedPaymentMethod(paymentMethod);
+    }, [],
+  );
+
+  return (
+    <Container>
+      <HeaderTitle>Select your payment method</HeaderTitle>
+
+      <PaymentMethodsContainer>
+        <AlternativeSelect
+          onChangeSelectedOption={(option) => handleChangePaymentMethod(option)}
+          options={mapAllPaymentMethodsToAlternativeSelectOption()}
+          defaultOptionId={paymentMethods[0].id}
+        />
+      </PaymentMethodsContainer>
+    </Container>
+  );
+}
+
+export default PaymentOptionsSelect;
---
+++ src/components/Checkout/PaymentOptions/PaymentMethodSelect/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 3rem;
+`;
+
+export const HeaderTitle = styled.h3`
+  font-weight: normal;
+  margin-bottom: 2rem;
+`;
+
+export const PaymentMethodsContainer = styled.div`
+  padding: 1.7rem;
+  border-radius: 9px;
+  background-color: #F7F7F7;
+
+  @media all and (min-width: 760px) {
+    padding: 0;
+  }
+`;
