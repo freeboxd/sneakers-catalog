3eb7a8097917706eb9bd4916300e295e2b34e072
Guilherme Nunes <guilherme.martins@plugg.to>
2021-04-09 01:14:33 -0300

    Created AlternativeSelect UI component

---
+++ src/components/UI/AlternativeSelect/index.tsx
@@ -0,0 +1,69 @@
+import React, { SetStateAction, useCallback, useState } from 'react';
+
+import {
+  Container,
+  Option,
+  OptionTitle,
+  Badge,
+  OptionLeft,
+} from './styles';
+
+export interface IAlternativeSelectOption {
+  iconSrc?: string,
+  badge?: string,
+  id: string,
+  title: string,
+}
+
+interface AlternativeSelectProps {
+  onChangeSelectedOption: (option: IAlternativeSelectOption) => void,
+  options: IAlternativeSelectOption[],
+  defaultOptionId?: string,
+}
+
+export const AlternativeSelect: React.FC<AlternativeSelectProps> = ({
+  options,
+  onChangeSelectedOption,
+  defaultOptionId,
+}: AlternativeSelectProps) => {
+  const [selectedOptionId, setSelectedOptionId] = useState<string>(null);
+
+  useState(() => {
+    setSelectedOptionId(defaultOptionId);
+  });
+
+  const handleChangeOption = useCallback(
+    (value: IAlternativeSelectOption) => {
+      setSelectedOptionId(value.id as SetStateAction<string>);
+      onChangeSelectedOption(value);
+    }, [],
+  );
+
+  return (
+    <Container>
+      {
+        options.length > 0 && (
+          <>
+            { options.map((option) => (
+              <Option
+                key={option.id}
+                onClick={() => handleChangeOption(option)}
+                isSelected={option.id === selectedOptionId}
+              >
+                <OptionLeft>
+                  <OptionTitle>
+                    {option.title}
+                  </OptionTitle>
+                  {option.badge !== undefined && <Badge>{option.badge}</Badge>}
+                </OptionLeft>
+                {option.iconSrc && <img src={option.iconSrc}></img> }
+              </Option>
+            ))}
+          </>
+        )
+      }
+    </Container>
+  );
+};
+
+export default AlternativeSelect;
---
+++ src/components/UI/AlternativeSelect/styles.ts
@@ -0,0 +1,68 @@
+import styled, { css } from 'styled-components';
+
+interface OptionProps {
+  onClick: () => void,
+  isSelected: boolean,
+}
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Option = styled.button<OptionProps>`
+  width: 100%;
+  background-color: #fff;
+  display: flex;
+  justify-content: space-between;
+  position: relative;
+  padding: 2rem;
+  height: 6rem;
+  border-radius: 9px;
+  outline: 0;
+  border: none;
+  cursor: pointer;
+  align-items: center;
+  font-size: 1.4rem;
+
+  ${props => props.isSelected && css`
+    border: solid 1px #63B55C;
+  `}
+
+  & + & {
+    margin-top: 2rem;
+  }
+`;
+
+export const Badge = styled.div`
+  justify-content: flex-start;
+  width: 60%;
+  font-size: 1.1rem;
+  text-transform: uppercase;
+  padding: .2rem 0;
+  font-weight: 700;
+  border-radius: 5px;
+  background-color: #FFCC00;
+  color: #9E7D27;
+`;
+
+
+export const OptionLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media all and (min-width: 769px) {
+    & ${Badge} {
+      position: absolute;
+      top: -10px;
+      width: auto;
+      padding: .2rem 1rem
+    }
+  }
+`;
+
+export const OptionTitle = styled.div`
+`;
+
+
+
