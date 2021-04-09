41938c6d257e870be32b00053e3af42bf6aadd63
Guilherme Nunes <guilherme.martins@plugg.to>
2021-04-09 01:00:47 -0300

    Added PaymentMethod interface

--- src/@types/payment/Payment.ts
+++ src/@types/payment/Payment.ts
@@ -1,6 +1,9 @@
+import { StyledIcon } from '@styled-icons/styled-icon';
+
 export interface IPaymentMethod {
   name: string,
   badge?: string,
   iconSrc?: string,
   id: string,
+  icon: StyledIcon,
 }
