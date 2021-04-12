0d03081cdf061a3cfeb4ffb6b9f63b3895963948
Guilherme Nunes <guilherme.martins@plugg.to>
2021-04-12 03:19:18 -0300

    Fixed Select Input styles

--- src/components/UI/Select/styles.ts
+++ src/components/UI/Select/styles.ts
@@ -21,4 +21,13 @@ export const SelectElement = styled.select<SelectProps>`
   padding-left: 1rem;
   margin-right: 1rem;
   color: #808080;
+
+  box-shadow: none;
+  outline: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  background-image: url(data:image/svg+xml;base64,PHN2ZyBmaWxsPSdibGFjaycgaGVpZ2h0PScyNCcgdmlld0JveD0nMCAwIDI0IDI0JyB3aWR0aD0nMjQnIHhtbG5zPSdodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2Zyc+PHBhdGggZD0nTTcgMTBsNSA1IDUtNXonLz48cGF0aCBkPSdNMCAwaDI0djI0SDB6JyBmaWxsPSdub25lJy8+PC9zdmc+);
+  background-repeat: no-repeat;
+  background-position-x: 42px;
+  background-position-y: 2px;
 `;
